8a37106050235da97e888211a31772367c22bf24
nafees nazik <nafeesnazik21@gmail.com>
2022-10-06 12:46:11 +0530

    feat: add i18n config

---
+++ next-i18next.config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+module.exports = {
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+  localePath: path.resolve("./locales"),
+};
