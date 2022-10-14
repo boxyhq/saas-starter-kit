16f404688edeb2bc2cba821652d0856d21295b7a
Saptarshee Bhowmick <sap.bho1110@gmail.com>
2022-10-14 07:14:29 +0000

    Updated Test script

--- tests/e2e/auth/login.spec.ts
+++ tests/e2e/auth/login.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 
 test("Should navigate to login page", async ({ page }) => {
   page.goto("/");
-  await expect(page).toHaveURL("http://localhost:4002/auth/login");
+  await expect(page).toHaveURL("/auth/login");
 });
