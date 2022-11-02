288448dee7d39b3752315c5b57d6b971dfa66930
Saptarshee Bhowmick <sap.bho1110@gmail.com>
2022-11-02 06:45:41 +0000

    Updated Pricing section design

--- components/ui/PricingSection.tsx
+++ components/ui/PricingSection.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Button, Card } from "react-daisyui";
+import { Button, Card, Checkbox } from "react-daisyui";
 import Spinner from "./Spinner";
 
 const PricingSection = ({ }) => {
@@ -12,13 +12,13 @@ const PricingSection = ({ }) => {
             setPricing(pricing.data);
             isLoading(false);
         })
-    },[]);
+    }, []);
 
     return (
         <Card className="border-none">
             <Card.Body className="items-center text-center">
                 <Card.Title tag="h2" className="normal-case text-4xl font-bold">Pricing</Card.Title>
-                { loading && <Spinner/>}
+                {loading && <Spinner />}
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                     {
                         pricing.map((price) => {
@@ -26,8 +26,19 @@ const PricingSection = ({ }) => {
                                 <div>
                                     <Card>
                                         <Card.Body className="items-center">
-                                            <Card.Title tag="h2">{price.amount}/{price.duration}</Card.Title>
+                                            <Card.Title tag="h2">{price.currency} {price.amount}/{price.duration}</Card.Title>
                                             <p>{price.description}</p>
+                                            <div className="grid grid-cols-1 gap-1"></div>
+                                            {
+                                                price.benefits.map((benefit: any) => {
+                                                    return (
+                                                        <div>
+                                                            <Checkbox checked color="primary" className="m-2" size="xs"/>
+                                                            {benefit}
+                                                        </div>
+                                                    )
+                                                })
+                                            }
                                         </Card.Body>
                                         <Card.Actions className="justify-center">
                                             <Button color="primary" className="w-full">Buy Now</Button>
