ab60106a1b2c10f525d28a1c1a4ddd67e3e97693
Allan <allanice001@users.noreply.github.com>
2023-06-09 01:15:45 +0100

    Update middleware.ts
    Fix middleware for settings to be hidden when not authenticated
    
    Signed-off-by: Allan <allanice001@users.noreply.github.com>

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/account/:path*', '/teams/:path*', '/dashboard'],
+  matcher: ['/settings/:path*', '/teams/:path*', '/dashboard'],
 };
