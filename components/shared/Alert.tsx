d4a0c28d9b66356430bb4ee5a5688258132e82cb
Kiran K <mailtokirankk@gmail.com>
2023-06-16 15:42:20 +0530

    Update Alert component

--- components/shared/Alert.tsx
+++ components/shared/Alert.tsx
@@ -2,15 +2,10 @@ import React from 'react';
 import { Alert as AlertCore, AlertProps } from 'react-daisyui';
 
 const Alert = (props: AlertProps) => {
-  const { children, status = "success", ...rest } = props;
-
-  const classes = {
-    danger: 'bg-red-100 border-red-400 text-red-700',
-    success: 'bg-green-100 border-green-400 text-green-700',
-  };
+  const { children, ...rest } = props;
 
   return (
-    <AlertCore {...rest} className={`rounded px-4 py-3 text-white ${classes[status]}`}>
+    <AlertCore {...rest} className={`rounded px-4 py-3`}>
       {children}
     </AlertCore>
   );
