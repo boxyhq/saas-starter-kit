c5f3f0ab32b7f3498021bd116c0d0885510ef4e1
Kiran K <mailtokirankk@gmail.com>
2023-06-16 11:19:37 +0530

    Add empty state placeholder

--- components/shared/EmptyState.tsx
+++ components/shared/EmptyState.tsx
@@ -1,9 +1,17 @@
+import { FolderPlusIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 
-const EmptyState = ({ title }: { title: string }) => {
+interface EmptyStateProps {
+  title: string;
+  description?: string;
+}
+
+const EmptyState = ({ title, description }: EmptyStateProps) => {
   return (
-    <div className="flex flex-col items-center justify-center space-y-3 rounded py-24">
-      <h3>{title}</h3>
+    <div className="flex w-full flex-col items-center justify-center rounded-md lg:p-20 border-2 border-dashed gap-3 bg-white h-80 border-slate-600">
+      <FolderPlusIcon className='w-12 h-12' />
+      <h3 className='text-semibold text-emphasis text-center text-xl'>{title}</h3>
+      {description && <p className='text-default text-center font-light leading-6'>{description}</p>}
     </div>
   );
 };
