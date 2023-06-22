dc9ce21a8e8a040fa992a1ead97e65bbfd48dea6
Kiran K <mailtokirankk@gmail.com>
2023-06-22 15:11:04 +0530

    Improve the middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,41 @@
-export { default } from 'next-auth/middleware';
+import micromatch from 'micromatch';
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// Add routes that don't require authentication
+const unAuthenticatedRoutes = [
+  '/api/hello',
+  '/api/auth/**',
+  '/api/oauth/**',
+  '/api/scim/v2.0/**',
+  '/auth/**',
+];
+
+export default async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  // Bypass routes that don't require authentication
+  if (micromatch.isMatch(pathname, unAuthenticatedRoutes)) {
+    return NextResponse.next();
+  }
+
+  const token = await getToken({
+    req,
+  });
+
+  // No token, redirect to signin page
+  if (!token) {
+    const url = new URL('/auth/login', req.url);
+    url.searchParams.set('callbackUrl ', encodeURI(req.url));
+
+    return NextResponse.redirect(url);
+  }
+
+  // All good, let the request through
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ['/settings/:path*', '/teams/:path*', '/dashboard'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
