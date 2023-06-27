0f5479b1ca127fc0efc0febeffa966cc500fba37
Kiran K <mailtokirankk@gmail.com>
2023-06-27 12:24:15 +0530

    Fix the type issue

--- core/components/api-keys/APIKeys.tsx
+++ core/components/api-keys/APIKeys.tsx
@@ -16,9 +16,10 @@ const APIKeys = ({ team }: APIKeysProps) => {
   const { t } = useTranslation('common');
 
   // Fetch API Keys
-  const { data, isLoading, error, mutate } = useSWR<{
-    data: ApiKey[];
-  }>(`/api/teams/${team.slug}/api-keys`, fetcher);
+  const { data, isLoading, error, mutate } = useSWR<{ data: ApiKey[] }>(
+    `/api/teams/${team.slug}/api-keys`,
+    fetcher
+  );
 
   // Delete API Key
   const deleteApiKey = async (id: string) => {
@@ -42,64 +43,62 @@ const APIKeys = ({ team }: APIKeysProps) => {
   const apiKeys = data?.data ?? [];
 
   return (
-    <>
-      <WithLoadingAndError isLoading={isLoading} error={error}>
-        {apiKeys.length === 0 ? (
-          <EmptyState
-            title="You haven't created any API Keys yet"
-            description="API Keys allow your app to communicate with BoxyHQ."
-          />
-        ) : (
-          <Card heading="API Keys">
-            <Card.Body>
-              <table className="w-full text-left text-sm text-gray-500">
-                <thead className="bg-gray-100 text-xs uppercase text-gray-700">
-                  <tr>
-                    <th scope="col" className="px-6 py-3">
-                      {t('name')}
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      {t('status')}
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      {t('created')}
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      {t('actions')}
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {apiKeys.map((apiKey) => {
-                    return (
-                      <tr key={apiKey.id} className="border-b bg-white">
-                        <td className="px-6 py-3">{apiKey.name}</td>
-                        <td className="px-6 py-3">
-                          <Badge color="success">Active</Badge>
-                        </td>
-                        <td className="px-6 py-3">
-                          {new Date(apiKey.createdAt).toLocaleDateString()}
-                        </td>
-                        <td className="px-6 py-3">
-                          <Button
-                            size="xs"
-                            color="error"
-                            variant="outline"
-                            onClick={() => deleteApiKey(apiKey.id)}
-                          >
-                            Remove
-                          </Button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </Card.Body>
-          </Card>
-        )}
-      </WithLoadingAndError>
-    </>
+    <WithLoadingAndError isLoading={isLoading} error={error}>
+      {apiKeys.length === 0 ? (
+        <EmptyState
+          title="You haven't created any API Keys yet"
+          description="API Keys allow your app to communicate with BoxyHQ."
+        />
+      ) : (
+        <Card heading="API Keys">
+          <Card.Body>
+            <table className="w-full text-left text-sm text-gray-500">
+              <thead className="bg-gray-100 text-xs uppercase text-gray-700">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    {t('name')}
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    {t('status')}
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    {t('created')}
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    {t('actions')}
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {apiKeys.map((apiKey) => {
+                  return (
+                    <tr key={apiKey.id} className="border-b bg-white">
+                      <td className="px-6 py-3">{apiKey.name}</td>
+                      <td className="px-6 py-3">
+                        <Badge color="success">Active</Badge>
+                      </td>
+                      <td className="px-6 py-3">
+                        {new Date(apiKey.createdAt).toLocaleDateString()}
+                      </td>
+                      <td className="px-6 py-3">
+                        <Button
+                          size="xs"
+                          color="error"
+                          variant="outline"
+                          onClick={() => deleteApiKey(apiKey.id)}
+                        >
+                          Remove
+                        </Button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </Card.Body>
+        </Card>
+      )}
+    </WithLoadingAndError>
   );
 };
 
