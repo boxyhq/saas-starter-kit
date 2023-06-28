06ef75b9a57f8db0010f2916e2e7bf7a16f69c29
Kiran K <mailtokirankk@gmail.com>
2023-06-28 15:21:02 +0530

    use Image from next/image

--- components/layouts/AuthLayout.tsx
+++ components/layouts/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import app from '@/lib/app';
+import Image from 'next/image';
 
 interface AuthLayoutProps {
   children: React.ReactNode;
@@ -15,10 +16,12 @@ export default function AuthLayout({
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div>
-          <img
+          <Image
             src={app.logoUrl}
             className="mx-auto h-12 w-auto"
             alt={app.name}
+            width={48}
+            height={48}
           />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             {heading}
