b3b85b4a401f20bb63cdc5cb0ef889590d8c4384
Aswin V <vaswin91@gmail.com>
2023-06-28 23:58:17 +0530

    Display list for sso connections

--- pages/teams/[slug]/saml.tsx
+++ pages/teams/[slug]/saml.tsx
@@ -1,7 +1,9 @@
+import { CreateConnection } from '@/components/saml';
 import { Alert, Error, InputWithLabel, Loading } from '@/components/shared';
 import { Card } from '@/components/shared';
-import { CreateConnection } from '@/components/saml';
+import ConfirmationDialog from '@/components/shared/ConfirmationDialog';
 import { TeamTab } from '@/components/team';
+import type { SAMLSSORecord } from '@boxyhq/saml-jackson';
 import useSAMLConfig from 'hooks/useSAMLConfig';
 import useTeam from 'hooks/useTeam';
 import { GetServerSidePropsContext } from 'next';
@@ -10,17 +12,49 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { Button } from 'react-daisyui';
-import type { NextPageWithLayout } from 'types';
+import { toast } from 'react-hot-toast';
+import type { ApiResponse, NextPageWithLayout } from 'types';
 
 const TeamSSO: NextPageWithLayout = () => {
   const router = useRouter();
   const { t } = useTranslation('common');
   const [visible, setVisible] = useState(false);
+  const [selectedSsoConnection, setSelectedSsoConnection] =
+    useState<SAMLSSORecord | null>(null);
+  const [confirmationDialogVisible, setConfirmationDialogVisible] =
+    useState(false);
 
   const { slug } = router.query as { slug: string };
 
   const { isLoading, isError, team } = useTeam(slug);
-  const { samlConfig } = useSAMLConfig(slug);
+  const { samlConfig, mutateSamlConfig } = useSAMLConfig(slug);
+  // Delete SSO Connection
+  const deleteSsoConnection = async (connection: SAMLSSORecord | null) => {
+    if (!connection) return;
+    const { clientID, clientSecret } = connection;
+    const params = new URLSearchParams({
+      clientID,
+      clientSecret,
+    });
+    const res = await fetch(`/api/teams/${slug}/saml?${params}`, {
+      method: 'DELETE',
+    });
+
+    const { data, error } = (await res.json()) as ApiResponse<null>;
+
+    setSelectedSsoConnection(null);
+    setConfirmationDialogVisible(false);
+
+    if (error) {
+      toast.error(error.message);
+      return;
+    }
+
+    if (data) {
+      mutateSamlConfig();
+      toast.success('SSO Connection deleted successfully');
+    }
+  };
 
   if (isLoading || !team) {
     return <Loading />;
@@ -30,11 +64,73 @@ const TeamSSO: NextPageWithLayout = () => {
     return <Error />;
   }
 
-  const connectionExists = samlConfig && 'idpMetadata' in samlConfig.config;
+  const connectionsAdded =
+    samlConfig?.connections && samlConfig.connections.length > 0;
 
   return (
     <>
       <TeamTab activeTab="saml" team={team} />
+      <div className="flex flex-col">
+        <div className="flex mt-2 justify-end">
+          <Button
+            color="primary"
+            onClick={() => {
+              setVisible(!visible);
+            }}
+          >
+            {t('add-connection')}
+          </Button>
+        </div>
+        <Card heading={t('team-connections')}>
+          <Card.Body>
+            <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+              <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  {/* <th scope="col" className="px-6 py-3">
+                    {t('name')}
+                  </th> */}
+                  <th scope="col" className="px-6 py-3">
+                    {t('provider')}
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    {t('actions')}
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {connectionsAdded &&
+                  samlConfig?.connections.map((connection) => {
+                    return (
+                      <tr
+                        key={connection.clientID}
+                        className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
+                      >
+                        <td className="px-6 py-3">
+                          {connection.idpMetadata.friendlyProviderName ||
+                            connection.idpMetadata.provider}
+                        </td>
+                        <td className="px-6 py-3">
+                          <Button
+                            size="xs"
+                            color="error"
+                            variant="outline"
+                            onClick={() => {
+                              setSelectedSsoConnection(connection);
+                              setConfirmationDialogVisible(true);
+                            }}
+                          >
+                            Delete
+                          </Button>
+                        </td>
+                        {/* <td className="px-6 py-3">{connection.product}</td> */}
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </Card.Body>
+        </Card>
+      </div>
       <Card heading={t('configure-singlesignon')}>
         <Card.Body className="px-3 py-3 text-sm">
           <div className="mb-3 flex items-center justify-between">
@@ -43,7 +139,7 @@ const TeamSSO: NextPageWithLayout = () => {
               {t('configure')}
             </Button>
           </div>
-          {connectionExists && (
+          {connectionsAdded && (
             <>
               <Alert status="success">{t('saml-connection-established')}</Alert>
               <div className="flex flex-col justify-between space-y-2 mt-4">
@@ -64,6 +160,17 @@ const TeamSSO: NextPageWithLayout = () => {
         </Card.Body>
       </Card>
       <CreateConnection team={team} visible={visible} setVisible={setVisible} />
+      <ConfirmationDialog
+        title="Delete SSO Connection"
+        visible={confirmationDialogVisible}
+        onConfirm={() => deleteSsoConnection(selectedSsoConnection)}
+        onCancel={() => setConfirmationDialogVisible(false)}
+        cancelText="Cancel"
+        confirmText="Delete SSO Connection"
+      >
+        Are you sure you want to delete this SSO Connection? This action can not
+        be undone.
+      </ConfirmationDialog>
     </>
   );
 };
