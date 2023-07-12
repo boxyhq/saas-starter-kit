020344104b41f53624e3bc35b2e75f8ab1319aa0
Aswin V <vaswin91@gmail.com>
2023-07-12 12:44:21 +0530

    Use `CreateSAMLConnection`

--- components/saml/CreateConnection.tsx
+++ components/saml/CreateConnection.tsx
@@ -1,16 +1,9 @@
-import { InputWithLabel } from '@/components/shared';
-import { getAxiosError } from '@/lib/common';
-import type { SAMLSSORecord } from '@boxyhq/saml-jackson';
+import { CreateSAMLConnection } from '@boxyhq/react-ui/sso';
 import { Team } from '@prisma/client';
-import axios from 'axios';
-import { useFormik } from 'formik';
 import useSAMLConfig from 'hooks/useSAMLConfig';
 import { useTranslation } from 'next-i18next';
-import { useState } from 'react';
-import { Button, Modal, Tabs, Textarea } from 'react-daisyui';
+import { Modal } from 'react-daisyui';
 import toast from 'react-hot-toast';
-import type { ApiResponse } from 'types';
-import * as Yup from 'yup';
 
 interface CreateConnectionProps {
   visible: boolean;
@@ -23,123 +16,58 @@ const CreateConnection = (props: CreateConnectionProps) => {
 
   const { mutateSamlConfig } = useSAMLConfig(team.slug);
   const { t } = useTranslation('common');
-  const [tab, setTab] = useState(0);
 
-  const initialValues = {
-    metadataUrl: '',
-    metadataRaw: '',
-  };
+  // const formik = useFormik({
+  //   initialValues,
+  //   validationSchema,
+  //   onSubmit: async (values, { resetForm }) => {
+  //     const { metadataUrl, metadataRaw } = values;
 
-  const validationSchema = Yup.object().shape({
-    metadataUrl: Yup.string()
-      .url()
-      .when('tab', {
-        is: tab === 0,
-        then(schema) {
-          return schema.required();
-        },
-      }),
-    metadataRaw: Yup.string().when('tab', {
-      is: tab === 1,
-      then(schema) {
-        return schema.required();
-      },
-    }),
-  });
+  //     try {
+  //       const response = await axios.post<ApiResponse<SAMLSSORecord>>(
+  //         `/api/teams/${team.slug}/saml`,
+  //         {
+  //           metadataUrl,
+  //           encodedRawMetadata: metadataRaw
+  //             ? Buffer.from(metadataRaw).toString('base64')
+  //             : undefined,
+  //         }
+  //       );
 
-  const formik = useFormik({
-    initialValues,
-    validationSchema,
-    onSubmit: async (values, { resetForm }) => {
-      const { metadataUrl, metadataRaw } = values;
+  //       const { data } = response.data;
 
-      try {
-        const response = await axios.post<ApiResponse<SAMLSSORecord>>(
-          `/api/teams/${team.slug}/saml`,
-          {
-            metadataUrl,
-            encodedRawMetadata: metadataRaw
-              ? Buffer.from(metadataRaw).toString('base64')
-              : undefined,
-          }
-        );
-
-        const { data } = response.data;
-
-        if (data) {
-          toast.success(t('saml-config-updated'));
-          mutateSamlConfig();
-          setVisible(false);
-          resetForm();
-        }
-      } catch (error: any) {
-        toast.error(getAxiosError(error));
-      }
-    },
-  });
+  //       if (data) {
+  //         toast.success(t('saml-config-updated'));
+  //         mutateSamlConfig();
+  //         setVisible(false);
+  //         resetForm();
+  //       }
+  //     } catch (error: any) {
+  //       toast.error(getAxiosError(error));
+  //     }
+  //   },
+  // });
 
   return (
     <Modal open={visible}>
-      <form onSubmit={formik.handleSubmit} method="POST">
-        <Modal.Header className="font-bold">
-          {t('configure-singlesignon')}
-        </Modal.Header>
-        <Modal.Body>
-          <div className="mt-4 flex flex-col space-y-4">
-            <Tabs variant="bordered" size="md" value={tab} onChange={setTab}>
-              <Tabs.Tab value={0}>{t('metadata-url')}</Tabs.Tab>
-              <Tabs.Tab value={1}>{t('metadata-xml')}</Tabs.Tab>
-            </Tabs>
-            {tab === 0 && (
-              <div className="flex">
-                <InputWithLabel
-                  label={t('metadata-url')}
-                  name="metadataUrl"
-                  onChange={formik.handleChange}
-                  value={formik.values.metadataUrl}
-                  required={true}
-                  error={formik.errors.metadataUrl}
-                />
-              </div>
-            )}
-
-            {tab === 1 && (
-              <div className="flex">
-                <Textarea
-                  name="metadataRaw"
-                  className="flex-grow"
-                  onChange={formik.handleChange}
-                  value={formik.values.metadataRaw}
-                  rows={6}
-                  placeholder={t('copy-paste-metadata')}
-                  required
-                />
-              </div>
-            )}
-          </div>
-        </Modal.Body>
-        <Modal.Actions>
-          <Button
-            type="submit"
-            color="primary"
-            loading={formik.isSubmitting}
-            active={formik.dirty}
-          >
-            {t('save-changes')}
-          </Button>
-          <Button
-            type="button"
-            variant="outline"
-            onClick={() => {
-              setVisible(!visible);
-              setTab(0);
-              formik.resetForm();
-            }}
-          >
-            {t('close')}
-          </Button>
-        </Modal.Actions>
-      </form>
+      <Modal.Header className="font-bold">
+        {t('configure-singlesignon')}
+      </Modal.Header>
+      <Modal.Body>
+        <CreateSAMLConnection
+          successCallback={() => {
+            setVisible(!visible);
+            mutateSamlConfig();
+          }}
+          t={t}
+          classNames={{ button: 'btn btn-primary' }}
+          errorCallback={(errMessage) => {
+            toast.error(errMessage);
+          }}
+          variant="basic"
+          urls={{ save: `/api/teams/${team.slug}/saml` }}
+        />
+      </Modal.Body>
     </Modal>
   );
 };
