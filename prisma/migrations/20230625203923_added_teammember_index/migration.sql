39edf40bbe4e746506d8d4beb375fd3592246477
Kiran K <kiran@boxyhq.com>
2023-07-12 15:51:45 +0530

    Fix the user index migration (#289)

--- prisma/migrations/20230625203923_added_teammember_index/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId]` on the table `TeamMember` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "TeamMember_userId_key" ON "TeamMember"("userId");
