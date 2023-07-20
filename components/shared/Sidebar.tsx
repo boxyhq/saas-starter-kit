2a8a57e1721939ffacd0fe4a7143af590c4f145c
Kiran K <mailtokirankk@gmail.com>
2023-07-20 12:46:28 +0530

    nested items in the sidebar

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,21 +1,20 @@
 import {
-  ChevronUpDownIcon,
   CodeBracketIcon,
   Cog6ToothIcon,
-  FolderIcon,
-  FolderPlusIcon,
   LockClosedIcon,
   RectangleStackIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
-import useTeams from 'hooks/useTeams';
-import { useSession } from 'next-auth/react';
 import { useTranslation } from 'next-i18next';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-import NavItem from './NavItem';
+import SidebarItem, { type SidebarMenuItem } from './SidebarItem';
+import TeamDropdown from './TeamDropdown';
+
+interface SidebarMenus {
+  [key: string]: SidebarMenuItem[];
+}
 
 export default function Sidebar() {
   const router = useRouter();
@@ -23,7 +22,7 @@ export default function Sidebar() {
 
   const { slug } = router.query;
 
-  const sidebarMenus = {
+  const sidebarMenus: SidebarMenus = {
     personal: [
       {
         name: t('all-teams'),
@@ -69,14 +68,27 @@ export default function Sidebar() {
               <TeamDropdown />
               <div className="p-4">
                 <ul className="space-y-1">
-                  {menus.map((item) => (
-                    <li key={item.name}>
-                      <NavItem
-                        href={item.href}
-                        text={t(item.name)}
-                        icon={item.icon}
-                        active={router.asPath === item.href}
+                  {menus.map((menu) => (
+                    <li key={menu.name}>
+                      <SidebarItem
+                        href={menu.href}
+                        name={t(menu.name)}
+                        icon={menu.icon}
+                        active={router.asPath === menu.href}
+                        items={menu.items}
                       />
+                      <div className="flex-1">
+                        <div className="mt-1 space-y-1">
+                          {menu?.items?.map((submenu) => (
+                            <SidebarItem
+                              href={submenu.href}
+                              name={submenu.name}
+                              active={router.asPath === submenu.href}
+                              className="pl-8"
+                            />
+                          ))}
+                        </div>
+                      </div>
                     </li>
                   ))}
                 </ul>
@@ -92,110 +104,3 @@ export default function Sidebar() {
     </>
   );
 }
-
-const TeamDropdown = () => {
-  const router = useRouter();
-
-  const { teams } = useTeams();
-  const { data } = useSession();
-  const { t } = useTranslation('common');
-
-  const currentTeam = (teams || []).find(
-    (team) => team.slug === router.query.slug
-  );
-
-  const menus = [
-    {
-      id: 2,
-      name: t('teams'),
-      items: (teams || []).map((team) => ({
-        id: team.id,
-        name: team.name,
-        href: `/teams/${team.slug}/settings`,
-        icon: FolderIcon,
-      })),
-    },
-    {
-      id: 1,
-      name: t('profile'),
-      items: [
-        {
-          id: data?.user.id,
-          name: data?.user?.name,
-          href: '/settings/account',
-          icon: UserCircleIcon,
-        },
-      ],
-    },
-    {
-      id: 3,
-      name: '',
-      items: [
-        {
-          id: 'all-teams',
-          name: t('all-teams'),
-          href: '/teams',
-          icon: RectangleStackIcon,
-        },
-        {
-          id: 'new-team',
-          name: t('new-team'),
-          href: '/teams?newTeam=true',
-          icon: FolderPlusIcon,
-        },
-      ],
-    },
-  ];
-
-  return (
-    <div className="px-4 py-2">
-      <div className="flex">
-        <div className="dropdown w-full">
-          <div
-            tabIndex={0}
-            className="border border-gray-300 flex h-10 items-center px-4 justify-between cursor-pointer rounded text-sm font-bold"
-          >
-            {currentTeam?.name || data?.user?.name}{' '}
-            <ChevronUpDownIcon className="w-5 h-5" />
-          </div>
-          <ul
-            tabIndex={0}
-            className="dropdown-content p-2 shadow-md bg-base-100 w-full rounded border px-2"
-          >
-            {menus.map(({ id, name, items }) => {
-              return (
-                <React.Fragment key={id}>
-                  {name && (
-                    <li
-                      className="text-xs text-gray-500 py-1 px-2"
-                      key={`${id}-name`}
-                    >
-                      {name}
-                    </li>
-                  )}
-                  {items.map((item) => (
-                    <li
-                      key={`${id}-${item.id}`}
-                      onClick={() => {
-                        if (document.activeElement) {
-                          (document.activeElement as HTMLElement).blur();
-                        }
-                      }}
-                    >
-                      <Link href={item.href}>
-                        <div className="flex hover:bg-gray-100 focus:bg-gray-100 focus:outline-none py-2 px-2 rounded text-sm font-medium gap-2 items-center">
-                          <item.icon className="w-5 h-5" /> {item.name}
-                        </div>
-                      </Link>
-                    </li>
-                  ))}
-                  {name && <li className="divider m-0" key={`${id}-divider`} />}
-                </React.Fragment>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
-};
