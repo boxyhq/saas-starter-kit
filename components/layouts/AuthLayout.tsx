e22a6679fb94d794d8815bd6dd5d95e62cac740f
Kiran K <mailtokirankk@gmail.com>
2023-07-24 11:02:35 +0530

    Fix the hook

--- components/layouts/AuthLayout.tsx
+++ components/layouts/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import app from '@/lib/app';
+import { useTranslation } from 'next-i18next';
 import Image from 'next/image';
 
 interface AuthLayoutProps {
@@ -12,6 +13,8 @@ export default function AuthLayout({
   heading,
   description,
 }: AuthLayoutProps) {
+  const { t } = useTranslation('common');
+
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
@@ -25,13 +28,11 @@ export default function AuthLayout({
           />
           {heading && (
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              {heading}
+              {t(heading)}
             </h2>
           )}
           {description && (
-            <p className="mt-2 text-center text-gray-600">
-              {description}
-            </p>
+            <p className="mt-2 text-center text-gray-600">{t(description)}</p>
           )}
         </div>
         {children}
--- pages/auth/verify-email.tsx
+++ pages/auth/verify-email.tsx
@@ -1,34 +1,15 @@
 import { AuthLayout } from '@/components/layouts';
 import { GetServerSidePropsContext } from 'next';
-import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import type { ReactElement } from 'react';
 
 const VerifyEmail = () => {
-  const { t } = useTranslation('common');
-
   return <></>
-
-  return (
-    <div className="rounded p-6 border">
-      <div className="space-y-3 text-center">
-        <h2>{t('confirm-email')}</h2>
-        <p className="text-base text-gray-600">
-          {t('confirm-email-description')}
-        </p>
-      </div>
-    </div>
-  );
 };
 
 VerifyEmail.getLayout = function getLayout(page: ReactElement) {
-  const { t } = useTranslation('common');
-
   return (
-    <AuthLayout
-      heading={t('confirm-email')}
-      description={t('confirm-email-description')}
-    >
+    <AuthLayout heading="confirm-email" description="confirm-email-description">
       {page}
     </AuthLayout>
   );
