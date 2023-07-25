5d954d4c2350cf29050fdcba3ffeb2269fbc31d1
Aswin V <vaswin91@gmail.com>
2023-07-25 11:19:17 +0530

    Minor formatting

--- components/saml/CreateConnection.tsx
+++ components/saml/CreateConnection.tsx
@@ -38,7 +38,6 @@ const CreateConnection = (props: CreateConnectionProps) => {
       <Modal.Body>
         <div className="mt-2 flex flex-col space-y-2">
           <p>{t('update-saml-config-message')}</p>
-
           <CreateSAMLConnection
             successCallback={() => {
               toast.success(t('saml-config-updated'));
