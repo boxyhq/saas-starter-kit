35f2054a1c7df18d73e0b8eeb1a1b61cde4585d5
Kiran K <mailtokirankk@gmail.com>
2023-08-22 15:55:45 +0530

    tweak the migration and schema

--- prisma/migrations/20230822102520_add_saml_jackson_schema/migration.sql
+++ prisma/migrations/20230822102520_add_saml_jackson_schema/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
 CREATE TABLE "jackson_store" (
-    "key" TEXT NOT NULL,
+    "key" VARCHAR(1500) NOT NULL,
     "value" TEXT NOT NULL,
-    "iv" TEXT NOT NULL,
-    "tag" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "iv" VARCHAR(64),
+    "tag" VARCHAR(64),
+    "createdAt" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(0),
 
     CONSTRAINT "jackson_store_pkey" PRIMARY KEY ("key")
 );
@@ -13,22 +13,28 @@ CREATE TABLE "jackson_store" (
 -- CreateTable
 CREATE TABLE "jackson_index" (
     "id" SERIAL NOT NULL,
-    "key" TEXT NOT NULL,
-    "storeKey" TEXT NOT NULL,
+    "key" VARCHAR(1500) NOT NULL,
+    "storeKey" VARCHAR(1500) NOT NULL,
 
     CONSTRAINT "jackson_index_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "jackson_ttl" (
-    "key" TEXT NOT NULL,
-    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "key" VARCHAR(1500) NOT NULL,
+    "expiresAt" BIGINT NOT NULL,
 
     CONSTRAINT "jackson_ttl_pkey" PRIMARY KEY ("key")
 );
 
 -- CreateIndex
 CREATE INDEX "jackson_index_key_idx" ON "jackson_index"("key");
+
+-- CreateIndex
+CREATE INDEX "jackson_index_key_storeKey_idx" ON "jackson_index"("key", "storeKey");
+
+-- CreateIndex
+CREATE INDEX "jackson_ttl_expiresAt_idx" ON "jackson_ttl"("expiresAt");
 
 -- AddForeignKey
 ALTER TABLE "jackson_index" ADD CONSTRAINT "jackson_index_storeKey_fkey" FOREIGN KEY ("storeKey") REFERENCES "jackson_store"("key") ON DELETE CASCADE ON UPDATE CASCADE;
