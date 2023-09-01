f28888d0c93209ad2251d39d7dcb8f9d45ad6dc9
Afolabi Moyinoluwa Emmanuel <afolabimoyinoluwa4@gmail.com>
2023-09-01 12:06:10 +0100

    useState Eslint

--- components/account/ProfileImageUpload.tsx
+++ components/account/ProfileImageUpload.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'next-i18next';
 import Image from 'next/image';
-import React, { ChangeEvent, useRef, useState } from 'react';
+import React, { ChangeEvent, useRef } from 'react';
 import { Button } from 'react-daisyui';
 
 const ProfileImageUpload = ({ formik }) => {
