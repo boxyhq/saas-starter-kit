e9cfe118f240ca16d04609bc0e111ae64624c579
Abdulrahman Yusuf <45533201+Young-Einstein10@users.noreply.github.com>
2023-09-04 13:08:43 +0100

    Rebuild the email templates using react.email  (#448)
    Co-authored-by: Kiran K <kiran@boxyhq.com>

---
+++ components/emailTemplates/TeamInvite.tsx
@@ -0,0 +1,44 @@
+import {
+  Button,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Text,
+} from '@react-email/components';
+import EmailLayout from './EmailLayout';
+import { Team } from '@prisma/client';
+
+interface TeamInviteEmailProps {
+  team: Team;
+  invitationLink: string;
+  userFirstname?: string;
+}
+
+const TeamInviteEmail = ({ team, invitationLink }: TeamInviteEmailProps) => {
+  return (
+    <Html>
+      <Head />
+      <Preview>Team Invitation</Preview>
+      <EmailLayout>
+        <Text>You have been invited to join the team at {team.name}.</Text>
+        <Text>
+          Click the link below to accept the invitation and join the team:
+        </Text>
+
+        <Container className="text-center">
+          <Button
+            href={invitationLink}
+            pX={20}
+            pY={16}
+            className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center"
+          >
+            Join team
+          </Button>
+        </Container>
+      </EmailLayout>
+    </Html>
+  );
+};
+
+export default TeamInviteEmail;
