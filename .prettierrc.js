10cdfa95d52911d75a596468272c2eb4aa1b4806
Deepak Prabhakara <deepak@boxyhq.com>
2023-09-27 17:28:15 +0100

    aligned prettier config with rest of our projects

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  semi: true,
+  printWidth: 110,
+  arrowParens: 'always',
+  importOrderSeparation: true,
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSortSpecifiers: true,
+};
