6e72a7fbd0e11c1b24a28ba1145859864ab08fbc
Kiran K <mailtokirankk@gmail.com>
2023-09-27 15:56:58 +0530

    Use getServerSideProps to redirect to login page if landing page is disabled

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 const { i18n } = require('./next-i18next.config');
 
-// Redirect to login page if landing page is hidden
-const hideLandingPage = process.env.HIDE_LANDING_PAGE === 'true' ? true : false;
-const redirects = [];
-
-if (hideLandingPage) {
-  redirects.push({
-    source: '/',
-    destination: '/auth/login',
-    permanent: true,
-  });
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -20,9 +8,6 @@ const nextConfig = {
     domains: ['boxyhq.com'],
   },
   i18n,
-  async redirects() {
-    return redirects;
-  },
   rewrites: async () => {
     return [
       {
