03b22605448360cdce49abdef8193ddcb174633a
Rohit <rohitmondall8000@gmail.com>
2023-10-02 21:57:21 +0530

    fix/authenticated-user-routed-to-dashboard-route

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,85 +15,85 @@ import useTheme from 'hooks/useTheme';
 import env from '@/lib/env';
 
 const Home: NextPageWithLayout = () => {
-    const { toggleTheme, selectedTheme } = useTheme();
-    const { t } = useTranslation('common');
-    const { data: session } = useSession()
+  const { toggleTheme, selectedTheme } = useTheme();
+  const { t } = useTranslation('common');
+  const { data: session } = useSession()
 
-    // redirect to dashboard route, if session is present
-    if (session) redirect("/dashboard")
+  // redirect to dashboard route, if session is present
+  if (session) redirect("/dashboard")
 
-    return (
-        <div className="container mx-auto">
-            <div className="navbar bg-base-100">
-                <div className="flex-1">
-                    <Link href="/" className="btn-ghost btn text-xl normal-case">
-                        BoxyHQ
-                    </Link>
-                </div>
-                <div className="flex-none">
-                    <ul className="menu menu-horizontal flex items-center gap-4">
-                        <li>
-                            <button
-                                className="bg-none p-0 w-10 h-10 rounded-lg flex items-center justify-center"
-                                onClick={toggleTheme}
-                            >
-                                <selectedTheme.icon className="w-5 h-5" />
-                            </button>
-                        </li>
-                        <li>
-                            <Link
-                                href="/auth/join"
-                                className="btn btn-primary btn-md text-white"
-                            >
-                                {t('sign-up')}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href="/auth/login"
-                                className="btn btn-primary btn-outline btn-md"
-                            >
-                                {t('sign-in')}
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <HeroSection />
-            <div className="divider"></div>
-            <FeatureSection />
-            <div className="divider"></div>
-            <PricingSection />
-            <div className="divider"></div>
-            <FAQSection />
+  return (
+    <div className="container mx-auto">
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <Link href="/" className="btn-ghost btn text-xl normal-case">
+            BoxyHQ
+          </Link>
         </div>
-    );
+        <div className="flex-none">
+          <ul className="menu menu-horizontal flex items-center gap-4">
+            <li>
+              <button
+                className="bg-none p-0 w-10 h-10 rounded-lg flex items-center justify-center"
+                onClick={toggleTheme}
+              >
+                <selectedTheme.icon className="w-5 h-5" />
+              </button>
+            </li>
+            <li>
+              <Link
+                href="/auth/join"
+                className="btn btn-primary btn-md text-white"
+              >
+                {t('sign-up')}
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/auth/login"
+                className="btn btn-primary btn-outline btn-md"
+              >
+                {t('sign-in')}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <HeroSection />
+      <div className="divider"></div>
+      <FeatureSection />
+      <div className="divider"></div>
+      <PricingSection />
+      <div className="divider"></div>
+      <FAQSection />
+    </div>
+  );
 };
 
 export const getServerSideProps = async (
-    context: GetServerSidePropsContext
+  context: GetServerSidePropsContext
 ) => {
-    // Redirect to login page if landing page is disabled
-    if (env.hideLandingPage) {
-        return {
-            redirect: {
-                destination: '/auth/login',
-                permanent: true,
-            },
-        }
-    }
-
-    const { locale } = context;
-
+  // Redirect to login page if landing page is disabled
+  if (env.hideLandingPage) {
     return {
-        props: {
-            ...(locale ? await serverSideTranslations(locale, ['common']) : {}),
-        },
-    };
+      redirect: {
+        destination: '/auth/login',
+        permanent: true,
+      },
+    }
+  }
+
+  const { locale } = context;
+
+  return {
+    props: {
+      ...(locale ? await serverSideTranslations(locale, ['common']) : {}),
+    },
+  };
 };
 
 Home.getLayout = function getLayout(page: ReactElement) {
-    return <>{page}</>;
+  return <>{page}</>;
 };
 
 export default Home;
