d03c1c9db986fc56583f35d1125da391adaa3c34
akg007 <aniruddhagawali03@gmail.com>
2023-10-07 20:29:35 +0530

    fix : added meta tag in HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import { type ReactElement } from 'react';
 import { useTranslation } from 'next-i18next';
@@ -17,50 +18,57 @@ const Home: NextPageWithLayout = () => {
   const { t } = useTranslation('common');
 
   return (
-    <div className="container mx-auto">
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <Link href="/" className="btn-ghost btn text-xl normal-case">
-            BoxyHQ
-          </Link>
+    <>
+      <Head>
+        <title>BoxyHQ</title>
+        <meta name="description">Enterprise SaaS Starter Kit</meta>
+      </Head>
+
+      <div className="container mx-auto">
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <Link href="/" className="btn-ghost btn text-xl normal-case">
+              BoxyHQ
+            </Link>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal flex items-center gap-4">
+              <li>
+                <button
+                  className="bg-none p-0 w-10 h-10 rounded-lg flex items-center justify-center"
+                  onClick={toggleTheme}
+                >
+                  <selectedTheme.icon className="w-5 h-5" />
+                </button>
+              </li>
+              <li>
+                <Link
+                  href="/auth/join"
+                  className="btn btn-primary btn-md text-white"
+                >
+                  {t('sign-up')}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/auth/login"
+                  className="btn btn-primary btn-outline btn-md"
+                >
+                  {t('sign-in')}
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal flex items-center gap-4">
-            <li>
-              <button
-                className="bg-none p-0 w-10 h-10 rounded-lg flex items-center justify-center"
-                onClick={toggleTheme}
-              >
-                <selectedTheme.icon className="w-5 h-5" />
-              </button>
-            </li>
-            <li>
-              <Link
-                href="/auth/join"
-                className="btn btn-primary btn-md text-white"
-              >
-                {t('sign-up')}
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/auth/login"
-                className="btn btn-primary btn-outline btn-md"
-              >
-                {t('sign-in')}
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <HeroSection />
+        <div className="divider"></div>
+        <FeatureSection />
+        <div className="divider"></div>
+        <PricingSection />
+        <div className="divider"></div>
+        <FAQSection />
       </div>
-      <HeroSection />
-      <div className="divider"></div>
-      <FeatureSection />
-      <div className="divider"></div>
-      <PricingSection />
-      <div className="divider"></div>
-      <FAQSection />
-    </div>
+    </>
   );
 };
 
@@ -68,13 +76,13 @@ export const getServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
   // Redirect to login page if landing page is disabled
-  if(env.hideLandingPage) {
+  if (env.hideLandingPage) {
     return {
       redirect: {
         destination: '/auth/login',
         permanent: true,
       },
-    }
+    };
   }
 
   const { locale } = context;
