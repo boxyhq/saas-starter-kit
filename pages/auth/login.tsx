5d3ce27c8324fe59d922f93f8fcd96b153e37ee2
Rohit <rohitmondall8000@gmail.com>
2023-10-10 16:47:01 +0530

    added meta tags

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -90,12 +90,11 @@ const Login: NextPageWithLayout<
   return (
     <>
       <Head>
-				<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<meta name="robots" content="index, follow" />
-				<meta name="keywords" content="Login to BoxyHQ's saas-starter-kit, saas-starter-kit, BoxyHQ saas-starter-kit login page, login saas-starter-kit, saas-starter-kit login user" />
-				<meta name="description" content="Welcome to BoxyHQ's saas-starter-kit login page. Resume your BoxyHQ's saas-starter-kit journey by clicking on 'Continue to Github button' or by entering your email and password." />
-				<title>Login to BoxyHQ&apos;s Saas Starter Kit</title>
+		    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		    <meta name="robots" content="index, follow" />
+		    <meta name="keywords" content="Login to BoxyHQ's saas-starter-kit, saas-starter-kit, BoxyHQ saas-starter-kit login page, login saas-starter-kit, saas-starter-kit login user" />				<meta name="description" content="Welcome to BoxyHQ's saas-starter-kit login page. Resume your BoxyHQ's saas-starter-kit journey by clicking on 'Continue to Github button' or by entering your email and password." />
+		    <title>Login to BoxyHQ&apos;s Saas Starter Kit</title>
 	    </Head>
 
       {message.text && message.status && (
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,13 @@
-import { Card } from '@/components/shared';
 import useTeams from 'hooks/useTeams';
 import { GetServerSidePropsContext } from 'next';
+import Head from 'next/head';
 import { useSession } from 'next-auth/react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import type { NextPageWithLayout } from 'types';
+
+import { Card } from '@/components/shared';
 
 const Dashboard: NextPageWithLayout = () => {
   const router = useRouter();
@@ -22,17 +24,28 @@ const Dashboard: NextPageWithLayout = () => {
   }
 
   return (
-    <Card>
-      <Card.Body>
-        <div className="p-3">
-          <p className="text-sm">
-            {`${t('hi')}, ${session?.user.name} ${t(
-              'you-have-logged-in-using'
-            )} ${session?.user.email}`}
-          </p>
-        </div>
-      </Card.Body>
-    </Card>
+    <>
+      <Head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="Saas-starter-kit dashboard, saas-starter-kit, BoxyHQ&apso;s saas-starter-kit, BoxyHQ" />
+        <meta name="description" content="Welcome to your dashboard for Saas-starter-kit of BoxyHQ. Here you will find everything related to your saas starter kit." />
+        <title>Dashboard || Saas-starter-kit</title>
+      </Head>
+
+      <Card>
+        <Card.Body>
+          <div className="p-3">
+            <p className="text-sm">
+              {`${t('hi')}, ${session?.user.name} ${t(
+                'you-have-logged-in-using'
+              )} ${session?.user.email}`}
+            </p>
+          </div>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
