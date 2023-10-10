2f8702e16c942f8ae2544b7361f39dfdf9753610
Kiran K <mailtokirankk@gmail.com>
2023-10-10 17:06:29 +0530

    Cleanup

--- lib/common.ts
+++ lib/common.ts
@@ -51,8 +51,33 @@ export const validateEmail = (email: string): boolean => {
   return re.test(email);
 };
 
-export const passwordPolicies = {
-  minLength: 8,
+export const validatePassword = (password: string): boolean => {
+  // Password should be at least 8 characters long
+  if (password.length < 8) {
+    return false;
+  }
+
+  // Password should have at least one lowercase letter
+  if (!/[a-z]/.test(password)) {
+    return false;
+  }
+
+  // Password should have at least one uppercase letter
+  if (!/[A-Z]/.test(password)) {
+    return false;
+  }
+
+  // Password should have at least one number
+  if (!/\d/.test(password)) {
+    return false;
+  }
+
+  // Password should have at least one special character
+  if (!/[^a-zA-Z0-9]/.test(password)) {
+    return false;
+  }
+
+  return true;
 };
 
 export const copyToClipboard = (text: string) => {
@@ -61,4 +86,8 @@ export const copyToClipboard = (text: string) => {
 
 export const defaultHeaders = {
   'Content-Type': 'application/json',
+};
+
+export const passwordPolicies = {
+  minLength: 8,
 };
