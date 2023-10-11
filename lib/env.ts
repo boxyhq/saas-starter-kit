515c05453a9acf3a02cdda58467492c098f7216c
Kiran K <mailtokirankk@gmail.com>
2023-10-11 17:18:14 +0530

    Add back redirectAfterSignIn

--- lib/env.ts
+++ lib/env.ts
@@ -2,6 +2,7 @@ const env = {
   databaseUrl: `${process.env.DATABASE_URL}`,
   appUrl: `${process.env.APP_URL}`,
   product: 'boxyhq',
+  redirectAfterSignIn: '/dashboard',
   redirectIfAuthenticated: '/dashboard',
 
   // SAML Jackson configuration
