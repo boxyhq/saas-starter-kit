7db627e670e0a0b77ba4ce4e4cab42bbb7ca5420
Aswin V <vaswin91@gmail.com>
2023-10-11 14:02:20 +0800

    Fix lint error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from 'next-auth/react';
 import { appWithTranslation } from 'next-i18next';
 import Head from 'next/head';
 import { Toaster } from 'react-hot-toast';
-import colors from 'tailwindcss/colors';
+// import colors from 'tailwindcss/colors';
 import type { AppPropsWithLayout } from 'types';
 import mixpanel from 'mixpanel-browser';
 
