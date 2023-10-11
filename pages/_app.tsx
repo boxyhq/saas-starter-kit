9cb0ab85791ddcbcb9f7cf2be95ba228b283d7af
Aswin V <vaswin91@gmail.com>
2023-10-11 13:56:49 +0800

    Disable theme override for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,20 +43,22 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       <SessionProvider session={session}>
         <Toaster toastOptions={{ duration: 4000 }} />
         <Themer
-          overrideTheme={{
-            '--primary-color': colors.blue['500'],
-            '--primary-hover': colors.blue['600'],
-            '--primary-color-50': colors.blue['50'],
-            '--primary-color-100': colors.blue['100'],
-            '--primary-color-200': colors.blue['200'],
-            '--primary-color-300': colors.blue['300'],
-            '--primary-color-500': colors.blue['500'],
-            '--primary-color-600': colors.blue['600'],
-            '--primary-color-700': colors.blue['700'],
-            '--primary-color-800': colors.blue['800'],
-            '--primary-color-900': colors.blue['900'],
-            '--primary-color-950': colors.blue['950'],
-          }}
+          overrideTheme={
+            {
+              // '--primary-color': colors.blue['500'],
+              // '--primary-hover': colors.blue['600'],
+              // '--primary-color-50': colors.blue['50'],
+              // '--primary-color-100': colors.blue['100'],
+              // '--primary-color-200': colors.blue['200'],
+              // '--primary-color-300': colors.blue['300'],
+              // '--primary-color-500': colors.blue['500'],
+              // '--primary-color-600': colors.blue['600'],
+              // '--primary-color-700': colors.blue['700'],
+              // '--primary-color-800': colors.blue['800'],
+              // '--primary-color-900': colors.blue['900'],
+              // '--primary-color-950': colors.blue['950'],
+            }
+          }
         >
           {getLayout(<Component {...props} />)}
         </Themer>
