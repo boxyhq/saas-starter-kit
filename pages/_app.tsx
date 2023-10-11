8a05b880ea79b32690e94c4d097c604b326f387d
Aswin V <vaswin91@gmail.com>
2023-10-11 14:31:20 +0800

    Revert comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from 'next-auth/react';
 import { appWithTranslation } from 'next-i18next';
 import Head from 'next/head';
 import { Toaster } from 'react-hot-toast';
-// import colors from 'tailwindcss/colors';
+import colors from 'tailwindcss/colors';
 import type { AppPropsWithLayout } from 'types';
 import mixpanel from 'mixpanel-browser';
 
@@ -43,22 +43,20 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       <SessionProvider session={session}>
         <Toaster toastOptions={{ duration: 4000 }} />
         <Themer
-          overrideTheme={
-            {
-              // '--primary-color': colors.blue['500'],
-              // '--primary-hover': colors.blue['600'],
-              // '--primary-color-50': colors.blue['50'],
-              // '--primary-color-100': colors.blue['100'],
-              // '--primary-color-200': colors.blue['200'],
-              // '--primary-color-300': colors.blue['300'],
-              // '--primary-color-500': colors.blue['500'],
-              // '--primary-color-600': colors.blue['600'],
-              // '--primary-color-700': colors.blue['700'],
-              // '--primary-color-800': colors.blue['800'],
-              // '--primary-color-900': colors.blue['900'],
-              // '--primary-color-950': colors.blue['950'],
-            }
-          }
+          overrideTheme={{
+            '--primary-color': colors.blue['500'],
+            '--primary-hover': colors.blue['600'],
+            '--primary-color-50': colors.blue['50'],
+            '--primary-color-100': colors.blue['100'],
+            '--primary-color-200': colors.blue['200'],
+            '--primary-color-300': colors.blue['300'],
+            '--primary-color-500': colors.blue['500'],
+            '--primary-color-600': colors.blue['600'],
+            '--primary-color-700': colors.blue['700'],
+            '--primary-color-800': colors.blue['800'],
+            '--primary-color-900': colors.blue['900'],
+            '--primary-color-950': colors.blue['950'],
+          }}
         >
           {getLayout(<Component {...props} />)}
         </Themer>
