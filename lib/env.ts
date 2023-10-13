811c423a939f21edc6996353d8490721b58500bc
Kiran K <mailtokirankk@gmail.com>
2023-10-13 11:14:04 +0530

    Rename the env variable

--- lib/env.ts
+++ lib/env.ts
@@ -73,7 +73,7 @@ const env = {
 
   hideLandingPage: process.env.HIDE_LANDING_PAGE === 'true' ? true : false,
 
-  darkModeEnabled: process.env.DARK_MODE_ENABLED === 'true' ? true : false,
+  darkModeEnabled: process.env.NEXT_PUBLIC_DARK_MODE === 'true' ? true : false,
 };
 
 export default env;
