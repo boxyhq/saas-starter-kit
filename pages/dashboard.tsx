1737237c22ddd9d719a5484c5ae77b0f341e6cb8
Aswin V <vaswin91@gmail.com>
2023-10-13 13:39:45 +0800

    Show loading state if teams are not loaded yet

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,18 +1,16 @@
-import { Card } from '@/components/shared';
+import { Loading } from '@/components/shared';
 import useTeams from 'hooks/useTeams';
 import { GetServerSidePropsContext } from 'next';
 import { useSession } from 'next-auth/react';
-import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, type ReactElement } from 'react';
 import type { NextPageWithLayout } from 'types';
 
 const Dashboard: NextPageWithLayout = () => {
   const router = useRouter();
   const { teams, isLoading } = useTeams();
-  const { t } = useTranslation('common');
-  const { data: session } = useSession();
+  const { status: sessionStatus } = useSession();
 
   useEffect(() => {
     if (isLoading || !teams) {
@@ -26,19 +24,15 @@ const Dashboard: NextPageWithLayout = () => {
     }
   }, [isLoading, router, teams]);
 
-  return (
-    <Card>
-      <Card.Body>
-        <div className="p-3">
-          <p className="text-sm">
-            {`${t('hi')}, ${session?.user.name} ${t(
-              'you-have-logged-in-using'
-            )} ${session?.user.email}`}
-          </p>
-        </div>
-      </Card.Body>
-    </Card>
-  );
+  if (isLoading || sessionStatus === 'loading' || !teams) {
+    return <Loading />;
+  }
+
+  return null;
+};
+
+Dashboard.getLayout = function getLayout(page: ReactElement) {
+  return page;
 };
 
 export async function getStaticProps({ locale }: GetServerSidePropsContext) {
