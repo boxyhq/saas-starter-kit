f23ca2b7b74d54ef4e6c45a4a1880a3fd1931d15
jatin <jatingodnani@gmail.com>
2023-10-15 11:36:21 +0530

    changes

--- components/webhook/EventTypes.tsx
+++ components/webhook/EventTypes.tsx
@@ -39,9 +39,9 @@ const EventTypes = ({
     <>
       {events}
       {error && typeof error === 'string' && (
-+        <div className="label-text-alt text-red-500">{error}</div>
-+      )}
-   </>
+        <div className="label-text-alt text-red-500">{error}</div>
+      )}
+    </>
   );
 };
 
