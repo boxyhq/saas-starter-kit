b63f0d5af98d19b9693ef7e99a107b78f525a013
shreyas0924 <shreyasp0924@gmail.com>
2023-10-16 22:31:39 +0530

    fix: updated borders for both dark and light mode

--- components/defaultLanding/FeatureSection.tsx
+++ components/defaultLanding/FeatureSection.tsx
@@ -5,7 +5,7 @@ import features from './data/features.json';
 const FeatureSection = () => {
   const { t } = useTranslation('common');
   return (
-    <section className="py-6 px-2">
+    <section className="py-6 px-2 ">
       <div className="flex flex-col justify-center space-y-6">
         <h2 className="text-center text-4xl font-bold normal-case">
           {t('features')}
@@ -14,12 +14,15 @@ const FeatureSection = () => {
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry.
         </p>
-        <div className="flex items-center justify-center">
-          <div className="grid grid-cols-1 xl:grid-cols-3 md:grid-cols-2">
+        <div className="flex items-center justify-center  ">
+          <div className="grid grid-cols-1 xl:grid-cols-3 md:grid-cols-2 gap-2">
             {features.map((feature: any, index) => {
               return (
-                <div className="card-compact card" key={index}>
-                  <div className="card-body">
+                <div
+                  className="card-compact card dark:border-gray-200 border border-gray-300"
+                  key={index}
+                >
+                  <div className="card-body ">
                     <h2 className="card-title">{feature.name}</h2>
                     <p>{feature.description}</p>
                   </div>
