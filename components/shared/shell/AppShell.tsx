f16393b6ac5dd4d7381064507c9311885032b9d7
Kiran K <kiran@boxyhq.com>
2023-11-01 17:06:46 +0530

    Tweak the components (#713)

--- components/shared/shell/AppShell.tsx
+++ components/shared/shell/AppShell.tsx
@@ -22,7 +22,7 @@ export default function AppShell({ children }) {
       <Drawer sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <div className="lg:pl-64">
         <Header setSidebarOpen={setSidebarOpen} />
-        <main className="py-10">
+        <main className="py-5">
           <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
             {children}
           </div>
