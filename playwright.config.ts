21cca886214106d80058fa0ae18dd8933e5de02c
Kiran K <kiran@boxyhq.com>
2023-11-16 13:43:45 +0530

    Update playwright.config.ts
    Signed-off-by: Kiran K <kiran@boxyhq.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,36 +6,20 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
   ],
-  reporter: [
-    [
-      'html',
-      {
-        outputFolder: 'report',
-        open: 'never',
-      },
-    ],
-  ],
+  reporter: 'html',
   webServer: {
-    command: 'npm run start',
+    command: 'npm run dev',
     url: 'http://localhost:4002',
-    reuseExistingServer: true,
-    env: {
-      NODE_ENV: 'test',
-      DATABASE_URL:
-        'postgresql://testuser:testpassword@localhost:5432/saas-starter-kit',
-    },
+    reuseExistingServer: !process.env.CI,
   },
   use: {
     headless: true,
     ignoreHTTPSErrors: true,
     baseURL: 'http://localhost:4002',
-    video: 'off',
+    video: "off"
   },
   testDir: './tests',
 };
+
 export default config;
