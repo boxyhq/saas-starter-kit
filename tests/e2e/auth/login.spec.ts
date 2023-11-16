87d81a21e222d825e7af6bc99635a16ae0cf8eb5
Kiran K <mailtokirankk@gmail.com>
2023-11-16 13:43:08 +0530

    Update login.spec.ts
    Signed-off-by: Kiran K <mailtokirankk@gmail.com>

--- tests/e2e/auth/login.spec.ts
+++ tests/e2e/auth/login.spec.ts
@@ -1,48 +1,52 @@
 import { expect, test } from '@playwright/test';
+
 import { prisma } from '@/lib/prisma';
 
-const userCred = {
-  name: 'Test',
-  team: 'Test Teamg',
-  email: 'test@gggxd.com',
-  password: 'Test123#',
-  emailVerified: new Date(),
-};
+const user = {
+  name: 'Jackson',
+  email: 'jackson+4@boxyhq.com',
+  password: 'password',
+} as const;
 
-test('Should navigate to login page', async ({ page }) => {
+const team = {
+  name: 'BoxyHQ',
+  slug: 'boxyhq',
+} as const;
+
+test('Should signup a new user', async ({ page }) => {
   await page.goto('/auth/join');
   await expect(page).toHaveURL('/auth/join');
   await expect(
     page.getByRole('heading', { name: 'Get started' })
   ).toBeVisible();
-  await page.getByPlaceholder('Your name').fill(userCred.name);
-  await page.getByPlaceholder('Team Name').fill(userCred.team);
-  await page.getByPlaceholder('example@boxyhq.com').fill(userCred.email);
-  await page.getByPlaceholder('Password').fill(userCred.password);
+  await page.getByPlaceholder('Your name').fill(user.name);
+  await page.getByPlaceholder('Team Name').fill(team.name);
+  await page.getByPlaceholder('example@boxyhq.com').fill(user.email);
+  await page.getByPlaceholder('Password').fill(user.password);
   await page.getByRole('button', { name: 'Create Account' }).click();
+  await page.waitForURL('/auth/login');
+  await page.waitForSelector(
+    'text=You have successfully created your account.'
+  );
+});
 
-  // Login
+test('Should login a user', async ({ page }) => {
+  await page.goto('/auth/login');
   await expect(page).toHaveURL('/auth/login');
   await expect(
     page.getByRole('heading', { name: 'Welcome back' })
   ).toBeVisible();
-  await page.getByPlaceholder('Email').fill(userCred.email);
-  await page.getByPlaceholder('Password').fill(userCred.password);
+  await page.getByPlaceholder('Email').fill(user.email);
+  await page.getByPlaceholder('Password').fill(user.password);
   await page.getByRole('button', { name: 'Sign in' }).click();
-  // await expect(page).toHaveURL(/\/teams\/.*\/settings/);
+  await page.waitForURL('/dashboard');
+  await page.waitForURL(`/teams/${team.slug}/settings`);
+  await page.waitForSelector('text=Settings');
 });
 
-test.afterEach(async () => {
-  // 1. Clean the database
-  const deleteUserDetails = prisma.user.delete({
-    where: {
-      email: userCred.email,
-    },
-  });
-  await prisma.$transaction([deleteUserDetails]);
-
-  // 2. Disconnect
+test.afterAll(async () => {
+  await prisma.teamMember.deleteMany();
+  await prisma.team.deleteMany();
+  await prisma.user.deleteMany();
   await prisma.$disconnect();
-
-  console.log('Account disconnected successfully');
 });
