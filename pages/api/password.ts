7feb0dea2265f049ab892f6fa74c3c3892cdae47
Kiran K <kiran@boxyhq.com>
2023-11-16 23:44:58 +0530

    Remove sessions when resetting or updating password (#758)

--- pages/api/password.ts
+++ pages/api/password.ts
@@ -8,6 +8,9 @@ import { getSession } from '@/lib/session';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ApiError } from 'next/dist/server/api-utils';
 import { recordMetric } from '@/lib/metrics';
+import { getCookie } from 'cookies-next';
+import { sessionTokenCookieName } from '@/lib/nextAuth';
+import env from '@/lib/env';
 
 export default async function handler(
   req: NextApiRequest,
@@ -56,6 +59,20 @@ const handlePUT = async (req: NextApiRequest, res: NextApiResponse) => {
     where: { id: session?.user.id },
     data: { password: await hashPassword(newPassword) },
   });
+
+  // Remove all sessions other than the current one
+  if (env.nextAuth.sessionStrategy === 'database') {
+    const sessionToken = getCookie(sessionTokenCookieName, { req, res });
+
+    await prisma.session.deleteMany({
+      where: {
+        userId: session?.user.id,
+        NOT: {
+          sessionToken,
+        },
+      },
+    });
+  }
 
   recordMetric('user.password.updated');
 
