3d20c9a34c91140693642bf29fda40f7f8485238
Deepak Prabhakara <deepak@boxyhq.com>
2023-11-18 17:47:35 +0000

    added sts, xframe and xcontenttype headers

--- next.config.js
+++ next.config.js
@@ -26,6 +26,27 @@ const nextConfig = {
   sentry: {
     hideSourceMaps: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*?)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains;',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 // Additional config options for the Sentry webpack plugin.
