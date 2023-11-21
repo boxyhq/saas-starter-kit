ca0e94d0a5a59f05a2b97278e80d4d5f9564def5
Kiran K <mailtokirankk@gmail.com>
2023-11-21 11:32:04 +0530

    Format

--- components/shared/shell/Brand.tsx
+++ components/shared/shell/Brand.tsx
@@ -4,7 +4,13 @@ import Image from 'next/image';
 const Brand = () => {
   return (
     <div className="flex pt-6 shrink-0 items-center text-xl font-bold gap-2">
-      <Image className="w-auto" src={app.logoUrl} alt={app.name} width={30} height={30} />
+      <Image
+        className="w-auto"
+        src={app.logoUrl}
+        alt={app.name}
+        width={30}
+        height={30}
+      />
       {app.name}
     </div>
   );
