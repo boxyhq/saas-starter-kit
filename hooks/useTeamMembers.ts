6f51f5abfce5ee2a253e266af585ebf5365d0cea
Kiran K <mailtokirankk@gmail.com>
2023-12-01 17:43:04 +0530

    When deleting invite or team member show the User or email in the confirmation dialog

--- hooks/useTeamMembers.ts
+++ hooks/useTeamMembers.ts
@@ -3,7 +3,7 @@ import type { TeamMember, User } from '@prisma/client';
 import useSWR, { mutate } from 'swr';
 import type { ApiResponse } from 'types';
 
-type TeamMemberWithUser = TeamMember & { user: User };
+export type TeamMemberWithUser = TeamMember & { user: User };
 
 const useTeamMembers = (slug: string) => {
   const url = `/api/teams/${slug}/members`;
