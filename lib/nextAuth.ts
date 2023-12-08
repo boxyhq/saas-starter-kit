310c775fd3918ade9a485b6854c6850008046c2b
Aswin V <vaswin91@gmail.com>
2023-12-08 17:51:52 +0530

    Add Credentials Provider to handle IdP login

--- lib/nextAuth.ts
+++ lib/nextAuth.ts
@@ -135,6 +135,81 @@ if (isAuthProviderEnabled('saml')) {
       httpOptions: {
         timeout: 30000,
       },
+    }),
+    CredentialsProvider({
+      id: 'boxyhq-idp',
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'IdP Login',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        code: {
+          label:
+            'Code: Go to https://mocksaml.com/saml/login to initiate SAML IdP login',
+          type: 'text',
+          placeholder: 'Enter code',
+        },
+      },
+      async authorize(credentials, req) {
+        const { code } = credentials || {};
+
+        if (!code) {
+          return null;
+        }
+
+        const samlLoginUrl = env.jackson.selfHosted
+          ? env.jackson.url
+          : env.appUrl;
+
+        const res = await fetch(`${samlLoginUrl}/api/oauth/token`, {
+          method: 'POST',
+          body: JSON.stringify({
+            grant_type: 'authorization_code',
+            client_id: 'dummy',
+            client_secret: 'dummy',
+            redirect_url: process.env.NEXTAUTH_URL,
+            code,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (res.status !== 200) {
+          return null;
+        }
+
+        const json = await res.json();
+        if (!json?.access_token) {
+          return null;
+        }
+
+        const resUserInfo = await fetch(`${samlLoginUrl}/api/oauth/userinfo`, {
+          headers: {
+            Authorization: `Bearer ${json.access_token}`,
+          },
+        });
+
+        if (resUserInfo.status !== 200) {
+          return null;
+        }
+        const profile = await resUserInfo.json();
+
+        if (profile?.id && profile?.email) {
+          return {
+            id: profile.id,
+            email: profile.email,
+            name: [profile.firstName, profile.lastName]
+              .filter(Boolean)
+              .join(' '),
+            image: null,
+          };
+        }
+
+        return null;
+      },
     })
   );
 }
