0cd15830a8ed3a643addb779fc00ce7c6bf44b61
Aswin V <vaswin91@gmail.com>
2023-12-11 14:06:45 +0530

    Add getLayout and refactor useEffect

--- pages/login/saml.tsx
+++ pages/login/saml.tsx
@@ -1,15 +1,24 @@
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { ReactElement, useEffect } from 'react';
 
-export default function Page() {
+export default function SAMLIdPLogin() {
   const router = useRouter();
 
-  useEffect(() => {
-    if (!router.isReady) return;
+  const { isReady, query } = router;
 
-    signIn('boxyhq-idp', { callbackUrl: '/', code: router.query?.code });
-  }, [router.isReady]);
+  useEffect(() => {
+    if (!isReady) return;
+
+    signIn('boxyhq-idp', {
+      callbackUrl: '/dashboard',
+      code: query?.code,
+    });
+  }, [isReady, query]);
 
   return null;
 }
+
+SAMLIdPLogin.getLayout = function getLayout(page: ReactElement) {
+  return <>{page}</>;
+};
