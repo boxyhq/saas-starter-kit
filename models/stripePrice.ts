f82d55db1563e9fa7c3088737ef0ca5bb00b37ab
ukrocks007 <ukrocks.mehta@gmail.com>
2024-01-02 18:55:55 +0530

    Refactor stripePrice and stripeProduct models

--- models/stripePrice.ts
+++ models/stripePrice.ts
@@ -1,35 +1,4 @@
 import { prisma } from '@/lib/prisma';
-import Stripe from 'stripe';
-
-export const createPrice = async (data: Stripe.Price) => {
-  const price = await prisma.stripePrice.create({
-    data: {
-      id: data.id,
-      billingScheme: data.billing_scheme,
-      created: new Date(data.created * 1000),
-      currency: data.currency,
-      customUnitAmount: data.custom_unit_amount
-        ? data.custom_unit_amount.toString()
-        : null,
-      livemode: data.livemode,
-      lookupKey: data.lookup_key,
-      metadata: data.metadata,
-      nickname: data.nickname,
-      productId: data.product as string,
-      recurring: (data.recurring || {}) as any,
-      tiersMode: data.tiers_mode ? data.tiers_mode.toString() : '',
-      type: data.type,
-      unitAmount: data.unit_amount ? data.unit_amount.toString() : null,
-      unitAmountDecimal: data.unit_amount_decimal,
-    },
-  });
-  return price;
-};
-
-export const deleteAllPrices = async () => {
-  const prices = await prisma.stripePrice.deleteMany({});
-  return prices;
-};
 
 export const getAllPrices = async () => {
   const prices = await prisma.stripePrice.findMany({});
--- models/stripeProduct.ts
+++ models/stripeProduct.ts
@@ -1,25 +1,4 @@
 import { prisma } from '@/lib/prisma';
-import Stripe from 'stripe';
-
-export const createProduct = async (data: Stripe.Product) => {
-  const product = await prisma.stripeProduct.create({
-    data: {
-      id: data.id,
-      description: data.description || '',
-      image: data.images.length > 0 ? data.images[0] : '',
-      metadata: data.metadata,
-      name: data.name,
-      unitLabel: data.unit_label,
-      created: new Date(data.created * 1000),
-    },
-  });
-  return product;
-};
-
-export const deleteAllProducts = async () => {
-  const products = await prisma.stripeProduct.deleteMany({});
-  return products;
-};
 
 export const getAllProducts = async () => {
   const products = await prisma.stripeProduct.findMany({});
