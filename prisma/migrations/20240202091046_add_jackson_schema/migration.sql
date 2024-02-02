16ace6862ae270737b33602a542a1991c988ad8d
Kiran K <mailtokirankk@gmail.com>
2024-02-02 14:46:06 +0530

    Update jackson schema migration and constraints

--- prisma/migrations/20240202091046_add_jackson_schema/migration.sql
+++ prisma/migrations/20240202091046_add_jackson_schema/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "jackson_store" (
+CREATE TABLE IF NOT EXISTS "jackson_store" (
     "key" VARCHAR(1500) NOT NULL,
     "value" TEXT NOT NULL,
     "iv" VARCHAR(64),
@@ -12,7 +12,7 @@ CREATE TABLE "jackson_store" (
 );
 
 -- CreateTable
-CREATE TABLE "jackson_index" (
+CREATE TABLE IF NOT EXISTS "jackson_index" (
     "id" SERIAL NOT NULL,
     "key" VARCHAR(1500) NOT NULL,
     "storeKey" VARCHAR(1500) NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE "jackson_index" (
 );
 
 -- CreateTable
-CREATE TABLE "jackson_ttl" (
+CREATE TABLE IF NOT EXISTS "jackson_ttl" (
     "key" VARCHAR(1500) NOT NULL,
     "expiresAt" BIGINT NOT NULL,
 
@@ -29,16 +29,29 @@ CREATE TABLE "jackson_ttl" (
 );
 
 -- CreateIndex
-CREATE INDEX "_jackson_store_namespace" ON "jackson_store"("namespace");
+CREATE INDEX IF NOT EXISTS "_jackson_store_namespace" ON "jackson_store"("namespace");
 
 -- CreateIndex
-CREATE INDEX "_jackson_index_key" ON "jackson_index"("key");
+CREATE INDEX IF NOT EXISTS "_jackson_index_key" ON "jackson_index"("key");
 
 -- CreateIndex
-CREATE INDEX "_jackson_index_key_store" ON "jackson_index"("key", "storeKey");
+CREATE INDEX IF NOT EXISTS "_jackson_index_key_store" ON "jackson_index"("key", "storeKey");
 
 -- CreateIndex
-CREATE INDEX "_jackson_ttl_expires_at" ON "jackson_ttl"("expiresAt");
+CREATE INDEX IF NOT EXISTS "_jackson_ttl_expires_at" ON "jackson_ttl"("expiresAt");
 
 -- AddForeignKey
-ALTER TABLE "jackson_index" ADD CONSTRAINT "jackson_index_storeKey_fkey" FOREIGN KEY ("storeKey") REFERENCES "jackson_store"("key") ON DELETE CASCADE ON UPDATE NO ACTION;
+DO $$ 
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM information_schema.table_constraints 
+        WHERE table_name = 'jackson_index' 
+        AND constraint_name = 'jackson_index_storeKey_fkey'
+    ) THEN
+        ALTER TABLE "jackson_index"
+        ADD CONSTRAINT "jackson_index_storeKey_fkey"
+        FOREIGN KEY ("storeKey") REFERENCES "jackson_store"("key")
+        ON DELETE CASCADE ON UPDATE NO ACTION;
+    END IF;
+END $$;
