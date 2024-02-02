1ab4a003a858b0ac908f2caf04a2f65b61443977
Kiran K <mailtokirankk@gmail.com>
2024-02-02 14:41:45 +0530

    Update migration

---
+++ prisma/migrations/20240202091046_add_jackson_schema/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "jackson_store" (
+    "key" VARCHAR(1500) NOT NULL,
+    "value" TEXT NOT NULL,
+    "iv" VARCHAR(64),
+    "tag" VARCHAR(64),
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modifiedAt" TIMESTAMP(6),
+    "namespace" VARCHAR(64),
+
+    CONSTRAINT "_jackson_store_key" PRIMARY KEY ("key")
+);
+
+-- CreateTable
+CREATE TABLE "jackson_index" (
+    "id" SERIAL NOT NULL,
+    "key" VARCHAR(1500) NOT NULL,
+    "storeKey" VARCHAR(1500) NOT NULL,
+
+    CONSTRAINT "_jackson_index_id" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "jackson_ttl" (
+    "key" VARCHAR(1500) NOT NULL,
+    "expiresAt" BIGINT NOT NULL,
+
+    CONSTRAINT "jackson_ttl_key" PRIMARY KEY ("key")
+);
+
+-- CreateIndex
+CREATE INDEX "_jackson_store_namespace" ON "jackson_store"("namespace");
+
+-- CreateIndex
+CREATE INDEX "_jackson_index_key" ON "jackson_index"("key");
+
+-- CreateIndex
+CREATE INDEX "_jackson_index_key_store" ON "jackson_index"("key", "storeKey");
+
+-- CreateIndex
+CREATE INDEX "_jackson_ttl_expires_at" ON "jackson_ttl"("expiresAt");
+
+-- AddForeignKey
+ALTER TABLE "jackson_index" ADD CONSTRAINT "jackson_index_storeKey_fkey" FOREIGN KEY ("storeKey") REFERENCES "jackson_store"("key") ON DELETE CASCADE ON UPDATE NO ACTION;
