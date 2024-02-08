fc31233a2f4d3a0f3d2f2a081a03ed4462084f45
Aswin V <vaswin91@gmail.com>
2024-02-08 22:15:11 +0530

    Padding fix for menu bar links

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,7 +44,7 @@ const Home: NextPageWithLayout = () => {
               <li>
                 <Link
                   href="/auth/join"
-                  className="btn btn-primary btn-md px-2 sm:px-4 text-white"
+                  className="btn btn-primary btn-md py-3 px-2 sm:px-4 text-white"
                 >
                   {t('sign-up')}
                 </Link>
@@ -52,7 +52,7 @@ const Home: NextPageWithLayout = () => {
               <li>
                 <Link
                   href="/auth/login"
-                  className="btn btn-primary dark:border-zinc-600 dark:border-2 dark:text-zinc-200 btn-outline px-2 sm:px-4 btn-md"
+                  className="btn btn-primary dark:border-zinc-600 dark:border-2 dark:text-zinc-200 btn-outline py-3 px-2 sm:px-4 btn-md"
                 >
                   {t('sign-in')}
                 </Link>
