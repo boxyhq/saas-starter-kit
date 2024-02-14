505acaa247aa65f4eca003db8030a8f71570a2ba
ukrocks007 <ukrocks.mehta@gmail.com>
2024-02-14 13:57:39 +0530

    Add indices to tables

--- prisma/migrations/20240213160517_indices/migration.sql
+++ prisma/migrations/20240213160517_indices/migration.sql
@@ -1,2 +1,11 @@
 -- CreateIndex
+CREATE INDEX "ApiKey_teamId_idx" ON "ApiKey"("teamId");
+
+-- CreateIndex
 CREATE INDEX "Invitation_email_idx" ON "Invitation"("email");
+
+-- CreateIndex
+CREATE INDEX "Invitation_teamId_idx" ON "Invitation"("teamId");
+
+-- CreateIndex
+CREATE INDEX "Session_userId_idx" ON "Session"("userId");
