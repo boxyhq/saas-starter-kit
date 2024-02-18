9b4fee437143d9dca832fa65e18ff245c9988e6c
saqibtanveer786 <adward797@gmail.com>
2024-02-18 11:34:32 +0500

    fix:Fixed the dark mode

--- components/shared/shell/Brand.tsx
+++ components/shared/shell/Brand.tsx
@@ -1,12 +1,14 @@
 import app from '@/lib/app';
 import Image from 'next/image';
+import useTheme from 'hooks/useTheme';
 
 const Brand = () => {
+  const { theme } = useTheme();
   return (
-    <div className="flex pt-6 shrink-0 items-center text-xl font-bold gap-2">
+    <div className="flex pt-6 shrink-0 items-center text-xl font-bold gap-2 dark:text-gray-100">
       <Image
         className="w-auto"
-        src={app.logoUrl}
+        src={theme !== 'dark' ? app.logoUrl : '/logowhite.png'}
         alt={app.name}
         width={30}
         height={30}
