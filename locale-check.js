55fa092282e44a1829fe68903507594e8ece1517
Deepak Prabhakara <deepak@boxyhq.com>
2024-02-21 19:43:39 +0000

    more locale checks specific to this repo, cleaned up unused strings

--- locale-check.js
+++ locale-check.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const regExp = /\bt\('(.*?)'/gm;
-const altRegExp = /\bi18nKey='(.*?)'/gm;
+const altRegExp = /\bi18nKey="(.*?)"/gm;
+// const authLayoutRegExp =
+//   /\bAuthLayout.*\bheading="(.*?)".*\bdescription="(.*?)"/gm;
+const authHeadingRegExp = /\bheading="(.*?)"/gm;
+const authDescriptionRegExp = /\bdescription="(.*?)"/gm;
 
 const allStrings = {};
 
@@ -19,24 +23,52 @@ files.forEach((file) => {
   }
 
   if (['.ts', '.tsx'].includes(path.extname(file.name).toLowerCase())) {
-    const fileContent = fs.readFileSync(path.join(file.path, file.name), 'utf8');
+    const fileContent = fs.readFileSync(
+      path.join(file.path, file.name),
+      'utf8'
+    );
 
     (fileContent.match(regExp) || []).forEach((match) => {
       const id = match.replace("t('", '').replace("'", '');
       // console.log('match:', match);
       allStrings[id] = true;
       if (!localeFile[id]) {
-        console.error(`Missing key: ${path.join(file.path, file.name)} - ${id}`);
+        console.error(
+          `Missing key: ${path.join(file.path, file.name)} - ${id}`
+        );
       }
     });
 
     (fileContent.match(altRegExp) || []).forEach((match) => {
-      const id = match.replace("i18nKey='", '').replace("'", '');
+      const id = match.replace('i18nKey="', '').replace('"', '');
       // console.log('match:', match, id);
       allStrings[id] = true;
       if (!localeFile[id]) {
-        console.error(`Missing key: ${path.join(file.path, file.name)} - ${id}`);
+        console.error(
+          `Missing key: ${path.join(file.path, file.name)} - ${id}`
+        );
       }
+    });
+
+    [authHeadingRegExp, authDescriptionRegExp].forEach((regExp) => {
+      const authGroups = fileContent.match(regExp) || [];
+      authGroups.forEach((match) => {
+        // console.log('match:', match);
+        const parts = match.replace('AuthLayout ', '');
+        parts.split(' ').forEach((part) => {
+          const id = part.startsWith('heading=')
+            ? part.replace('heading="', '').replace('"', '')
+            : part.replace('description="', '').replace('"', '');
+          // console.log('part:', part, id);
+
+          allStrings[id] = true;
+          if (!localeFile[id]) {
+            console.error(
+              `Missing key: ${path.join(file.path, file.name)} - ${id}`
+            );
+          }
+        });
+      });
     });
   }
 });
