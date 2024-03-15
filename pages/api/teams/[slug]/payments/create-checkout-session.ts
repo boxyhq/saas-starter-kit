e27bd130a293c81854bddec82f485c3485f1661e
ukrocks007 <ukrocks.mehta@gmail.com>
2024-03-15 16:02:57 +0530

    Update Stripe subscription and create checkout session logic

--- pages/api/teams/[slug]/payments/create-checkout-session.ts
+++ pages/api/teams/[slug]/payments/create-checkout-session.ts
@@ -32,15 +32,18 @@ const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
   const teamMember = await throwIfNoTeamAccess(req, res);
   const session = await getSession(req, res);
   const customerId = await getStripeCustomerId(teamMember, session);
+  const price = req.body.priceId;
+
+  // For metered billing, do not pass quantity
+  const quantity = req.body.quantity || undefined;
 
   const checkoutSession = await stripe.checkout.sessions.create({
     customer: customerId,
     mode: 'subscription',
     line_items: [
       {
-        price: req.body.priceId,
-        // For metered billing, do not pass quantity
-        quantity: req.body.quantity || undefined,
+        price,
+        quantity,
       },
     ],
 
