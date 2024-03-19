bdcf2aa7f3b690ccc5b180adbc0a2c222c6b59b6
Deepak Prabhakara <deepak@boxyhq.com>
2024-03-19 21:18:32 +0000

    format fix

--- pages/api/teams/[slug]/payments/create-checkout-session.ts
+++ pages/api/teams/[slug]/payments/create-checkout-session.ts
@@ -38,7 +38,7 @@ const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
   const teamMember = await throwIfNoTeamAccess(req, res);
   const session = await getSession(req, res);
   const customer = await getStripeCustomerId(teamMember, session);
-  
+
   const checkoutSession = await stripe.checkout.sessions.create({
     customer,
     mode: 'subscription',
