e865a62c39dc7099bb5263f067544ca3647987a1
Musyoka Philip <musyonchez@gmail.com>
2024-03-29 02:15:23 +0300

    refactor(team): inline immediately returned variable in getTeamMember function (#1188)

--- models/team.ts
+++ models/team.ts
@@ -155,7 +155,7 @@ export const getTeams = async (userId: string) => {
 };
 
 export async function getTeamRoles(userId: string) {
-  const teamRoles = await prisma.teamMember.findMany({
+  return await prisma.teamMember.findMany({
     where: {
       userId,
     },
@@ -164,8 +164,6 @@ export async function getTeamRoles(userId: string) {
       role: true,
     },
   });
-
-  return teamRoles;
 }
 
 // Check if the user is an admin or owner of the team
@@ -407,7 +405,7 @@ Execution Time: 0.050 ms
 
 // Get the current user's team member object
 export const getTeamMember = async (userId: string, slug: string) => {
-  const teamMember = await prisma.teamMember.findFirstOrThrow({
+  return await prisma.teamMember.findFirstOrThrow({
     where: {
       userId,
       team: {
@@ -421,8 +419,6 @@ export const getTeamMember = async (userId: string, slug: string) => {
       team: true,
     },
   });
-
-  return teamMember;
 };
 
 // Get current user with team info
