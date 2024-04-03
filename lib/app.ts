f1b703a7f7638c261f45e5aa35738bb191718119
ukrocks007 <ukrocks.mehta@gmail.com>
2024-04-03 23:36:18 +0530

    Update app.ts to use environment variable for URL

--- lib/app.ts
+++ lib/app.ts
@@ -1,10 +1,11 @@
 import packageInfo from '../package.json';
+import env from './env';
 
 const app = {
   version: packageInfo.version,
   name: 'BoxyHQ',
   logoUrl: 'https://boxyhq.com/img/logo.png',
-  url: 'http://localhost:4002',
+  url: env.appUrl,
 };
 
 export default app;
