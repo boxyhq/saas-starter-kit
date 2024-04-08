c22d55ed5bae3ba30c9590e4bd863f7a2d1f7a24
Musyonchez <musyonchez@gmail.com>
2024-04-08 03:35:32 +0300

    feat: 404 and 500 error pages

--- pages/500.tsx
+++ pages/500.tsx
@@ -3,29 +3,22 @@ import { AccountLayout } from '@/components/layouts';
 import { useTranslation } from 'react-i18next';
 import { GetServerSidePropsContext } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import Link from 'next/link';
 import router from 'next/router';
 
-const Custom404 = () => {
+const Custom500 = () => {
   const { t } = useTranslation('common');
   return (
     <div className="w-full items-center justify-center lg:px-2 xl:px-0 text-center dark:bg-black">
       <p className="text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider dark:text-gray-300">
-        {t('error-404')}
+        {t('error-500')}
       </p>
       <p className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-wider dark:text-gray-300 mt-2">
-        {t('page-not-found')}
+        {t('internal-server-error')}
       </p>
       <p className="text-lg md:text-xl lg:text-2xl dark:text-gray-500 my-12">
-        {t('sorry-not-found')}
+        {t('unable-to-find')}
       </p>
       <div className="mt-8 space-x-5">
-        <Link
-          href="/"
-          className="btn btn-primary btn-md py-3 px-2 sm:px-4 text-white"
-        >
-          {t('go-home')}
-        </Link>
         <button
           onClick={(e) => {
             e.preventDefault();
@@ -35,14 +28,17 @@ const Custom404 = () => {
         >
           {t('go-back')}
         </button>
+        <p className="text-lg md:text-xl lg:text-2xl dark:text-gray-500 my-12">
+          {t('try-again-later')}
+        </p>
       </div>
     </div>
   );
 };
 
-export default Custom404;
+export default Custom500;
 
-Custom404.getLayout = function getLayout(page: ReactElement) {
+Custom500.getLayout = function getLayout(page: ReactElement) {
   return <AccountLayout>{page}</AccountLayout>;
 };
 
