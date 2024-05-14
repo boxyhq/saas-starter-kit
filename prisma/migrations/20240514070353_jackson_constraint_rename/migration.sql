f29f62852e5686ab27255aaa9d7573f34ffb320b
ukrocks007 <ukrocks.mehta@gmail.com>
2024-05-14 17:57:38 +0530

    chore: Refactor jackson_index foreign key constraint renaming logic

--- prisma/migrations/20240514070353_jackson_constraint_rename/migration.sql
+++ prisma/migrations/20240514070353_jackson_constraint_rename/migration.sql
@@ -1,2 +1,13 @@
 -- AlterTable
-ALTER TABLE jackson_index RENAME CONSTRAINT "jackson_index_storeKey_fkey" TO "FK_937b040fb2592b4671cbde09e83";
+DO $$
+BEGIN 
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.table_constraints
+        WHERE table_name = 'jackson_index'
+        AND constraint_name = 'FK_937b040fb2592b4671cbde09e83'
+    ) THEN
+        ALTER TABLE jackson_index
+        RENAME CONSTRAINT "jackson_index_storeKey_fkey" TO "FK_937b040fb2592b4671cbde09e83";
+    END IF;
+END $$;
