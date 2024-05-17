3f6558fee7f0d4b49db495f169d7d3f69190111d
Utkarsh Mehta <ukrocks.mehta@gmail.com>
2024-05-17 07:16:37 +0530

    Update instrumentation.ts

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,17 +3,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 export function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    Sentry.init({
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-      tracesSampleRate: parseFloat(
-        process.env.NEXT_PUBLIC_SENTRY_TRACE_SAMPLE_RATE ?? '0.0'
-      ),
-      debug: false,
-    });
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
+  if (process.env.NEXT_RUNTIME === 'nodejs' || process.env.NEXT_RUNTIME === 'edge') {
     Sentry.init({
       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
       tracesSampleRate: parseFloat(
