62025f9775a1b6a931beb481371fcb2be54c6704
ukrocks007 <ukrocks.mehta@gmail.com>
2024-05-17 11:05:42 +0530

    chore: Refactor instrumentation.ts for better readability

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,7 +3,10 @@
 import * as Sentry from '@sentry/nextjs';
 
 export function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs' || process.env.NEXT_RUNTIME === 'edge') {
+  if (
+    process.env.NEXT_RUNTIME === 'nodejs' ||
+    process.env.NEXT_RUNTIME === 'edge'
+  ) {
     Sentry.init({
       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
       tracesSampleRate: parseFloat(
