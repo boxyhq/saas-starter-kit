08b8439152cacd1496d9658d350c3b38de6c20ce
Aswin V <vaswin91@gmail.com>
2024-06-07 17:10:17 +0530

    Associate label with input using id

--- components/shared/InputWithCopyButton.tsx
+++ components/shared/InputWithCopyButton.tsx
@@ -10,15 +10,18 @@ interface InputWithCopyButtonProps extends InputProps {
 const InputWithCopyButton = (props: InputWithCopyButtonProps) => {
   const { label, value, description, ...rest } = props;
 
+  const id = label.replace(/ /g, '');
+
   return (
     <div className="form-control w-full">
       <div className="flex justify-between items-center">
-        <label className="label pl-0">
+        <label className="label pl-0" htmlFor={id}>
           <span className="label-text">{label}</span>
         </label>
         <CopyToClipboardButton value={value?.toString() || ''} />
       </div>
       <Input
+        id={id}
         className="input input-bordered w-full text-sm"
         {...rest}
         value={value}
