5a06d26c10c80c44c1acaaa7dc91a24f10e1a7b0
Deepak Prabhakara <deepak@boxyhq.com>
2024-11-12 16:51:23 +0000

    removed experimental flags

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { esmExternals: false, webpackBuildWorker: true },
   reactStrictMode: true,
   images: {
     remotePatterns: [
