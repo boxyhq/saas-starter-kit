77c6f03328f077efba85b6ea8b268e9f388938b2
Deepak Prabhakara <deepak@boxyhq.com>
2024-12-01 13:47:51 +0000

    Revert "added `experimental: { esmExternals: true },` flag"
    This reverts commit 1d589cd90f2d74721725469714aab245529132e5.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { esmExternals: true },
   reactStrictMode: true,
   images: {
     remotePatterns: [
