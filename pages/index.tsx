dc0f994a3c5525fa54ba797e3ad520e0d97e642d
marcoan83 <marcoan83@gmail.com>
2025-06-05 15:35:12 +0200

    Update branding to GDPRcheck360

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const Home: NextPageWithLayout = () => {
         <div className="navbar bg-base-100 px-0 sm:px-1">
           <div className="flex-1">
             <Link href="/" className="btn btn-ghost text-xl normal-case">
-              BoxyHQ
+              GDPRcheck360
             </Link>
           </div>
           <div className="flex-none">
